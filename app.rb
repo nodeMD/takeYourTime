f854bc8492f3367a89aef2a53eb8654cdbee81a5
Maciej Dynieski <53050193+nodeMD@users.noreply.github.com>
2025-05-14 15:17:11 +0200

    code: add user creation

---
+++ app.rb
@@ -0,0 +1,64 @@
+require "sinatra"
+require "sinatra/activerecord"
+require "./models/user"
+require "./models/need"
+require "./models/want"
+require_relative "controllers/needs_controller"
+require_relative "controllers/wants_controller"
+use NeedsController
+use WantsController
+
+set :database_file, "config/database.yml"
+enable :sessions
+
+helpers do
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+end
+
+get "/" do
+  erb :index
+end
+
+get "/signup" do
+  @user = User.new
+  erb :signup
+end
+
+post "/signup" do
+  @user = User.new(nickname: params[:nickname], password: params[:password], password_confirmation: params[:password_confirmation])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/app"
+  else
+    erb :signup
+  end
+end
+
+get "/login" do
+  @error = nil
+  erb :login
+end
+
+post "/login" do
+  user = User.find_by(nickname: params[:nickname])
+  if user&.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/app"
+  else
+    @error = "Invalid nickname or password"
+    erb :login
+  end
+end
+
+get "/logout" do
+  session.clear
+  redirect "/"
+end
+
+get "/app" do
+  redirect "/" unless current_user
+  @title = "Welcome #{current_user.nickname}"
+  erb :app
+end
---
+++ models/user.rb
@@ -0,0 +1,10 @@
+require "bcrypt"
+
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :needs, dependent: :destroy
+  has_many :wants, dependent: :destroy
+
+  validates :nickname, presence: true, uniqueness: true, length: {minimum: 5}
+  validates :password, length: {minimum: 8}, if: -> { new_record? || !password.nil? }
+end
