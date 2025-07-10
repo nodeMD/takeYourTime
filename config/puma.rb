0d89b4e16266eb8e88f4fed1e38662f3289310e7
Maciej Dynieski <53050193+nodeMD@users.noreply.github.com>
2025-07-10 20:51:12 +0200

    fix db connection

--- config/puma.rb
+++ config/puma.rb
@@ -8,8 +8,24 @@ port        ENV['PORT']     || 10000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Worker specific setup
+  # Reconnect to the database
   if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
+    ActiveRecord::Base.establish_connection(
+      adapter:  'postgresql',
+      database: ENV['DB_NAME'],
+      username: ENV['DB_USERNAME'],
+      password: ENV['DB_PASSWORD'],
+      host:     ENV['DB_HOST'],
+      port:     ENV['DB_PORT'] || 5432,
+      sslmode:  'require',
+      sslrootcert: ENV['DB_SSLROOTCERT'] || './config/supabase-ca-cert.crt',
+      prepared_statements: false
+    )
+  end
+end
+
+before_fork do
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection_pool.disconnect!
   end
 end
