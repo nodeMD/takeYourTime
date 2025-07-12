f8dbedcd1d0d3291e2676828648e16db1e151fa5
Maciej Dynieski <53050193+nodeMD@users.noreply.github.com>
2025-07-12 19:56:58 +0200

    update item position after delete

--- models/checklist_item.rb
+++ models/checklist_item.rb
@@ -2,16 +2,26 @@ class ChecklistItem < ActiveRecord::Base
   belongs_to :checklist
 
   validates :name, presence: true, length: {maximum: 200}
-  validates :position, presence: true, uniqueness: {scope: :checklist_id}
-
+  validates :position, presence: true, uniqueness: { scope: :checklist_id }
+  
   before_validation :set_position, on: :create
-
+  after_destroy :reorder_positions
+  
   private
-
+  
   def set_position
     return if position.present? || checklist.blank?
-
+    
     last_position = checklist.checklist_items.maximum(:position) || 0
     self.position = last_position + 1
+  end
+  
+  def reorder_positions
+    checklist.checklist_items
+            .where('position > ?', position)
+            .order(:position)
+            .each do |item|
+      item.update_column(:position, item.position - 1)
+    end
   end
 end
